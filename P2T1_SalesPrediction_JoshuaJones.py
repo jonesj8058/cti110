e9a288927902be82c21a4e98287f6f0f0d46f226
jonesj8058 <55468282+jonesj8058@users.noreply.github.com>
2019-09-17 20:23:01 -0400

    Add files via upload
    "First submission"

---
+++ P2T1_SalesPrediction_JoshuaJones.py
@@ -0,0 +1,14 @@
+
+# Calculating profit from the total sales
+#09/11/2019
+#CTI-110 P2T1-Sales Prediction
+#Joshua Jones
+
+total_sales = float(input("Enter the projected sales:"))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print("The profit is $", format(profit, ",.2f"))
+input()
