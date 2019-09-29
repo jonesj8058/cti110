a8b91023d360a55faa5fe28f5e81f1737606f511
jonesj8058 <55468282+jonesj8058@users.noreply.github.com>
2019-09-29 13:34:15 -0400

    Add files via upload

---
+++ P3HW1_MonthNum_JoshuaJones.py
@@ -0,0 +1,42 @@
+
+# CTI-110
+# P3HW1 - Month number
+# Joshua Jones
+# 09/25/2019
+# script for display the number of the month
+
+# input of the month's number being displayed
+month_number = input("Enter the month's number:")
+# if and elif statments stating if the number equals to that specific number to print that month                    
+if month_number == "1":
+    print("month is January")
+elif month_number == "2":
+    print("month is February")
+elif month_number == "3":
+    print("month is March")
+elif month_number == "4":
+    print("month is April")
+elif month_number == "5":
+    print("month is May")
+elif month_number == "6":
+    print("month is June")
+elif month_number == "7":
+    print("month is July")
+elif month_number == "8":
+    print("month is August")
+elif month_number == "9":
+    print("month is September")
+elif month_number == "10":
+    print("month is October")
+elif month_number == "11":
+    print("month is November")
+elif month_number == "12":
+    print("month is December")
+# else statment stating if the incorrect number is input an error message will be displayed    
+else:
+    print("error wrong number entered")       
+                    
+           
+                     
+                     
+                     
